84ed81e032bdb04c1a29f1bc03198463ea962a40
Simarpreet Kaur <97946641+simarpreetxk@users.noreply.github.com>
2022-01-18 17:36:07 +0530

    Add files via upload

---
+++ 2. variables/2. number-type/script.js
@@ -0,0 +1,18 @@
+let int = 1
+let float = 5.7
+
+console.log(typeof int) //number
+console.log(typeof float) //number
+console.log(int+float) //6.7
+console.log(3*int) //3
+
+// Operation on Numbers
+let a=1
+let b=2
+let c=3
+console.log((a+b)/c) 
+
+// Floating Numbers
+let d=1.2
+let e=2.2
+console.log(d+e)
