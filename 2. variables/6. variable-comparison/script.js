c07cabe681123fe149050a4758f8f6aa535de39f
Simarpreet Kaur <97946641+simarpreetxk@users.noreply.github.com>
2022-01-21 14:10:49 +0530

    Add files via upload

---
+++ 2. variables/6. variable-comparison/script.js
@@ -0,0 +1,32 @@
+// Numbers
+
+let a = 1
+let b = 2
+
+console.log(a == b)
+console.log(a > b)
+console.log(a < b)
+console.log(a <= b)
+console.log(a >= b)
+
+// Strings
+
+let c = 'a'
+let d = 'b'
+
+console.log(c < d) //sorts things out alphabetically
+
+// Boolean
+
+let e = false
+let f = false
+
+console.log(e == f) //true
+
+// Null and Undefined
+
+let g = null
+let h = undefined
+
+console.log(g == h) //true
+// They represent the exact same thing; lack of a value = lack of a value(true)
